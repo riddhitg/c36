4af648cf8f23318622b68cdd375416901ad591a4
riddhitg <61347574+riddhitg@users.noreply.github.com>
2020-05-10 11:19:56 +0530

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,33 @@
+class Form{
+    constructor(){
+        this.input=createInput("name");
+        this.button=createButton('play');
+        this.greeting=createElement('h3');
+    }
+    hide(){
+        this.greeting.hide();
+        this.input.hide();
+        this.button.hide();
+    }
+    display(){
+        var title=createElement('h3');
+        title.html("car racing game");
+        title.position(130,0);
+
+        
+        this.input.position(330,160);
+        this.button.position(250,200);
+
+        this.button.mousePressed(()=>{
+            this.input.hide();
+            this.button.hide();
+            player.name=this.input.value();
+            playerCount+=1;
+            player.index=playerCount;
+            player.update();
+            player.updateCount(playerCount);
+            this.greeting.html("hello "+player.name);
+            this.greeting.position(430,160);
+        })
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,25 @@
+var gameState=0;
+var playerCount,database;
+var form,game,player;
+var allPlayers;
+
+function setup(){
+    createCanvas(500,500);
+    database = firebase.database();
+    game=new Game();
+    game.getState();
+    game.start();
+}
+
+function draw(){
+    background("white");
+    if(playerCount===4){
+        game.update(1);
+    }
+    if(gameState===1){
+        clear();
+        game.play();
+    }
+   
+
+}
